f813fc97701748a7a45b81db1d83bc215e365d75
Nimalan <nimalan@liminalinsights.com>
2024-05-08 07:50:56 -0700

    Even more customizations

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -22,8 +22,8 @@ return {
             -- tables with the `name` key will be registered with which-key if it's installed
             -- this is useful for naming menus
             ["<Leader>b"] = { name = "Buffers" },
-            -- quick save
             -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+            -- quick save
 
             ["<Leader>w"] = {
               name = "Windows",
